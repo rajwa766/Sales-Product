47798083adaa2508cf20e3c3e8c519694de5e1be
viky096 <viky09695@gmail.com>
2017-12-22 16:36:06 +0500

    profile page
    Profile page done

--- frontend/views/user/view.php
+++ frontend/views/user/view.php
@@ -12,9 +12,37 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    
 
-    <p>
+   
+
+    <div class="row lg-padding">
+        <div class="col-lg-3 ">
+            <div class="profile">
+                <img src="http://localhost:8080/sales-management-system/frontend/web/images/placeholder-profile.png" class="img-circle img-inline add-border" height="250"><br/>
+
+                <div class="username"><strong>@ <?php echo $model->username; ?></strong><br/>
+                    Member Since  <?php echo $model->created_at; ?>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-7">
+            <div class="other-details">
+            <h3><strong>Other Profile Info</strong></h3>
+            <ul class="profile-details">
+                <li><i class="fa fa-envelope"></i>&nbsp <?php echo $model->email; ?></li>
+                <li><i class="fa fa-phone"></i>&nbsp <?php echo $model->phone_no; ?></li>
+                <li><i class="fa fa-globe"></i>&nbsp <?php echo $model->link; ?></li>
+                <li><i class="fa fa-level-up"></i>&nbsp <?php echo $model->user_level_id; ?></li>
+                <li><i class="fa fa-home"></i>&nbsp <?php echo $model->city; ?></li>
+                <li><i class="fa fa-flag-o"></i>&nbsp <?php echo $model->country; ?></li>
+                <li><i class="fa fa-map-marker"></i>&nbsp <?php echo $model->address; ?></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+     <p>
         <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
@@ -24,27 +52,5 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
         ]) ?>
     </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'username',
-            // 'auth_key',
-            // 'password_hash',
-            // 'password_reset_token',
-            'email:email',
-            'status',
-            // 'created_at',
-            'link',
-            // 'updated_at',
-            'parent_id',
-            'user_level_id',
-            'phone_no',
-            'address',
-            'city',
-            'country',
-        ],
-    ]) ?>
 
 </div>
