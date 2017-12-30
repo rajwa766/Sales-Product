81b56b276f77e226b3bfc51b6bfd4b5f9b619022
Bahmad92 <bashir.ahmad1909@gmail.com>
2017-12-30 17:01:57 +0500

    frondend changed by bashir

--- frontend/views/user/_form_customer.php
+++ frontend/views/user/_form_customer.php
@@ -17,22 +17,38 @@ use kartik\select2\Select2;
            ); ?>
     <div class="row no-margin">
     <div class="col-md-6">
-    <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
-    
+        <div class="col-md-4">
+            First Name
+        </div>
+        <div class="col-md-8">
+    <?= $form->field($model, 'first_name')->textInput(['maxlength' => true])->label(false) ?>
+    </div>
     </div>
     <div class="col-md-6">
-    <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
-    
+        <div class="col-md-4">
+            Last Name
+        </div>
+        <div class="col-md-8">
+    <?= $form->field($model, 'last_name')->textInput(['maxlength' => true])->label(false) ?>
+    </div>
     </div>
 </div>   
     <div class="row no-margin">
     <div class="col-md-6">
-    <?= $form->field($model, 'username')->textInput(['maxlength' => true,'readonly' => !$model->isNewRecord]) ?>
-    
+        <div class="col-md-4">
+           Username
+        </div>
+        <div class="col-md-8">
+    <?= $form->field($model, 'username')->textInput(['maxlength' => true,'readonly' => !$model->isNewRecord])->label(false) ?>
+    </div>
     </div>
     <div class="col-md-6">
-    <?= $form->field($model, 'password')->textInput(['maxlength' => true]) ?>
-    
+        <div class="col-md-4">
+            Password
+        </div>
+        <div class="col-md-8">
+    <?= $form->field($model, 'password')->textInput(['maxlength' => true])->label(false) ?>
+    </div>
     </div>
 </div>    
 
@@ -40,33 +56,63 @@ use kartik\select2\Select2;
   
     <div class="row no-margin">
     <div class="col-md-6">
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-    
+        <div class="col-md-4">
+            Email
+        </div>
+        <div class="col-md-8">
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true])->label(false) ?>
+    </div>
     </div>
     <div class="col-md-6">
-    <?= $form->field($model, 'phone_no')->textInput(['maxlength' => true]) ?>
+        <div class="col-md-4">
+            Phone No
+        </div>
+        <div class="col-md-8">
+    <?= $form->field($model, 'phone_no')->textInput(['maxlength' => true])->label(false) ?>
+</div>
     </div>
 </div>   
     
      <div class="row no-margin">
     <div class="col-md-6">
-    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+        <div class="col-md-4">
+            Address
+        </div>
+        <div class="col-md-8">
+    <?= $form->field($model, 'address')->textInput(['maxlength' => true])->label(false) ?>
+</div>
     
     </div>
     <div class="col-md-6">
-    <?= $form->field($model, 'city')->textInput() ?>
+        <div class="col-md-4">
+            City
+        </div>
+        <div class="col-md-8">
+    <?= $form->field($model, 'city')->textInput()->label(false) ?>
+</div>
     </div>
     </div>
     
     <div class="row no-margin">
         <div class="col-md-6">
-    <?= $form->field($model, 'country')->textInput(['readonly' =>true,'value'=>'Thailand ']) ?>
+            <div class="col-md-4">
+            Country
+        </div>
+        <div class="col-md-8">
+    <?= $form->field($model, 'country')->textInput(['readonly' =>true,'value'=>'Thailand '])->label(false) ?>
+</div>
     </div>
 </div>  
 
+<div class="row no-margin">
+        <div class="col-md-6">
+            <div class="col-md-4">
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
+</div>
+</div>
+</div>
 
     <?php ActiveForm::end(); ?>
 
--- frontend/views/user/view.php
+++ frontend/views/user/view.php
@@ -15,35 +15,39 @@ $this->params['breadcrumbs'][] = $this->title;
     
 
    
+<div class="container">
+    <div class="row">
+        
+        <div class="col-md-7">
+            <div class="other-details">
+            <h3><strong><?php echo $model->first_name .'&nbsp;'. $model->last_name; ?></strong></h3>
+            <ul class="profile_detail">
+                <li><i class="fa fa-envelope"></i><?php echo $model->email; ?></li>
+                <li><i class="fa fa-phone"></i> <?php echo $model->phone_no; ?></li>
+                <li><i class="fa fa-globe"></i> <?php echo $model->link; ?></li>
+                <li><i class="fa fa-level-up"></i> <?php echo $model->user_level_id; ?></li>
+                <li><i class="fa fa-home"></i> <?php echo $model->city; ?></li>
+                <li><i class="fa fa-flag-o"></i> <?php echo $model->country; ?></li>
+                <li><i class="fa fa-map-marker"></i> <?php echo $model->address; ?></li>
+                <li><i class="fa fa-external-link" aria-hidden="true"></i><?= Yii::$app->homeUrl; ?>/order/create?id=<?= $model->id ?></li>
+            </ul>
+        </div>
+    </div>
+    <div class="col-md-5">
 
-    <div class="row lg-padding">
-        <div class="col-lg-4 ">
-            <div class="profile">
+            <div class="profile2">
                 <center><img src="<?= Yii::$app->homeUrl; ?>images/avatar-1.png" class="img-circle img-inline add-border" height="200"></center><br/>
 
                 <div class="username"><strong>@ <?php echo $model->username; ?></strong><br/>
                     Member Since  <?php echo $model->created_at; ?>
                 </div>
             </div>
-        </div>
-        <div class="col-lg-7">
-            <div class="other-details">
-            <h3><strong><?php echo $model->first_name .'&nbsp;'. $model->last_name; ?></strong></h3>
-            <ul class="profile-details">
-                <li><i class="fa fa-envelope"></i>&nbsp <?php echo $model->email; ?></li>
-                <li><i class="fa fa-phone"></i>&nbsp <?php echo $model->phone_no; ?></li>
-                <li><i class="fa fa-globe"></i>&nbsp <?php echo $model->link; ?></li>
-                <li><i class="fa fa-level-up"></i>&nbsp <?php echo $model->user_level_id; ?></li>
-                <li><i class="fa fa-home"></i>&nbsp <?php echo $model->city; ?></li>
-                <li><i class="fa fa-flag-o"></i>&nbsp <?php echo $model->country; ?></li>
-                <li><i class="fa fa-map-marker"></i>&nbsp <?php echo $model->address; ?></li>
-                <li><?= Yii::$app->homeUrl; ?>/order/create?id=<?= $model->id ?></li>
-            </ul>
-        </div>
+      
     </div>
 </div>
-
-     <p>
+</div>
+   <div class="row">
+    <div class="col-md-7 col-btn">
         <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
@@ -52,6 +56,6 @@ $this->params['breadcrumbs'][] = $this->title;
                 'method' => 'post',
             ],
         ]) ?>
-    </p>
-
+    </div>
+</div>
 </div>
